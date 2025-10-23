6a4d7a0d2ff58e7eb60a94c105db8540b0199a4d
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-23 22:37:32 +0200

    consumimos api de fakeapi store y creamos cartas de cada producto

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/Models/producto.model';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  urlFetch: string = 'https://fakestoreapi.com/products';
+  arrayProductos: Producto[] = [];
 
+  constructor() { }
+
+  async FetchProductos(): Promise<void> {
+    try {
+      const res = await fetch(this.urlFetch);
+      const data = await res.json();
+      this.arrayProductos = data;
+      console.log(this.arrayProductos)
+    } catch (error) {
+      console.error('Ha ocurrido el siguiente error: ', error)
+    }
+  }
+
+  ngOnInit(): void {
+    this.FetchProductos()
+  }
 }
